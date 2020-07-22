f872588fd178aeb32f1066789260a43e26e1f65a
TehShrike <me@JoshDuff.com>
2020-07-22 14:42:35 -0500

    Make it so

---
+++ index.js
@@ -0,0 +1,95 @@
+const pify = require(`pify`)
+const { readFile, writeFile } = pify(require(`fs`))
+const { join: joinPath } = require(`path`)
+const { get } = require(`httpie`)
+const makeDir = require(`make-dir`)
+const filenamify = require(`filenamify`)
+
+const catchify = promise => promise.then(result => [ null, result ]).catch(err => [ err, null ])
+
+// https://www.diigo.com/api_dev
+const buildUrl = ({ count, step, apiKey, user }) => `https://secure.diigo.com/api/v2/bookmarks?key=${ apiKey }&user=${ user }&start=${ step * count }&count=${ count }&sort=1&filter=all`
+
+const buildHeaders = ({ user, password }) => ({
+	Authorization: `Basic ${ Buffer.from(`${ user }:${ password }`).toString(`base64`) }`,
+})
+
+const removeProtocol = urlString => {
+	const [ , ...rest ] = urlString.split(`://`)
+	return rest.join(``)
+}
+
+const diigoAndUserContentSeparator = `---\n`
+const extension = `.md`
+
+const stepper = async(increment, fn) => {
+	let current = 0
+	let shouldContinue = true
+
+	while (shouldContinue) {
+		shouldContinue = await fn(current)
+		++current
+	}
+}
+
+const main = async({ user, password, path, all, apiKey, countPerRequest }) => {
+	const headers = buildHeaders({ user, password })
+	const count = parseInt(countPerRequest, 10)
+
+	await makeDir(path)
+
+	await stepper(countPerRequest, async step => {
+		const { data: bookmarks } = await get(buildUrl({ count, step, apiKey, user }), { headers })
+
+		await updateBookmarksOnDisc({ bookmarks, path })
+
+		return all && bookmarks.length === countPerRequest
+	})
+}
+
+const updateBookmarksOnDisc = async({ bookmarks, path }) => Promise.all(bookmarks.map(
+	async({ tags, url, title, created_at: createdAt, desc: description }) => {
+		const sanitizedName = filenamify(removeProtocol(url), {
+			replacement: `_`,
+			maxLength: 255 - extension.length,
+		})
+		const fullPath = joinPath(path, sanitizedName + extension)
+		const diigoData = noteContents({ tags, url, title, createdAt })
+
+		const [ err, currentContents ] = await catchify(readFile(fullPath, { encoding: `utf8` }))
+
+		if (err && err.code === `ENOENT`) {
+			await writeFile(fullPath, diigoData + `\n` + description + `\n`)
+		} else if (err) {
+			throw err
+		} else {
+			const [ , ...rest ] = currentContents.split(diigoAndUserContentSeparator)
+			await writeFile(fullPath, diigoData + rest.join(diigoAndUserContentSeparator))
+		}
+	},
+))
+
+const noteContents = ({ tags, url, title, createdAt }) =>
+	`# ${ title }
+
+- tags: ${ tags.split(/,\s*/g).map(tag => `#${ tag }`).join(` `) }
+- url: ${ url }
+- cached: [On Diigo](https://www.diigo.com/cached?url=${ encodeURIComponent(url) })
+- created: [[${ new Date(createdAt).toISOString().slice(0, 10) }]]
+
+---
+`
+
+const args = require(`mri`)(process.argv.slice(2), {
+	string: [ `user`, `password`, `path`, `apiKey`, `countPerRequest` ],
+	boolean: `all`,
+	default: {
+		all: false,
+		countPerRequest: 20,
+	},
+})
+
+main(args).catch(err => {
+	console.error(err)
+	process.exit(1)
+})
---
+++ readme.md
@@ -0,0 +1,51 @@
+Sync all your Diigo bookmarks to a directory as Markdown files.  Intended for use with [Obsidian](https://obsidian.md/).
+
+## Install
+
+```sh
+npm i -g sync-diigo-to-folder
+```
+
+## Use
+
+```sh
+sync-diigo-to-folder --path=~/Obsidian/Bookmarks --all --user=DIIGO_USERNAME --password=DIIGO_PASSWORD --apiKey=DIIGO_API_KEY
+```
+
+This script is meant to be idempotent, so that you can re-run it over and over without losing any data other than what originally came from Diigo.
+
+By default it only reads the most recently-updated batch of bookmarks.
+
+### Arguments
+
+- `path`: the directory to write output files to
+- `all`: *(default off)* – whether to save the most recently-updated bookmarks, or only one request's worth
+- `countPerRequest`: *(default 20)* – How many bookmarks to fetch per API request.  Max 100.
+- `user`: your Diigo username
+- `password`: your Diigo password
+- `apiKey`: your [Diigo API key](https://www.diigo.com/api_keys/new/)
+
+## Output
+
+Right now the output for a bookmark of a site like <https://danluu.com/corp-eng-blogs/> looks like:
+
+```md
+# How (some) good corporate engineering blogs are written
+
+- tags: #writing #marketing #blogging
+- url: https://danluu.com/corp-eng-blogs/
+- cached: [On Diigo](https://www.diigo.com/cached?url=https%3A%2F%2Fdanluu.com%2Fcorp-eng-blogs%2F)
+- created: [[2020-07-13]]
+
+---
+
+
+```
+
+the intention is that you can put your own notes below the `---` separator as desired.  Any changes above the separator will be overwritten by changes to your bookmark in Diigo.
+
+If you typed a description into Diigo, that description will be placed below the `---` separator on first write.  Updated descriptions will not be written to a pre-existing file.
+
+## License
+
+[WTFPL](https://wtfpl2.com)
